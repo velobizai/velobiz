d2fdfc5b3130a8780681d16a161b63931961609e
jparekh <jparekh@1-stop.biz>
2026-02-26 00:03:49 +1100

    point api to azure

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: '/api',
+  production: true,
+  apiUrl: 'https://velobizapi-d4fxdehebtbvgwhb.australiasoutheast-01.azurewebsites.net/api',
   social: {
     linkedin: 'https://linkedin.com/company/velocityai',
     twitter: 'https://twitter.com/velocityai',
